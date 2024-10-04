526bb25eca95550ded4e7dbd57fbb7069d619981
Hussain <perryscott1516@gmail.com>
2024-10-04 03:19:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// const hello = document.querySelector("#scorl-Button");
+
+//   window.addEventListener("scroll", () => {
+//     if (window.pageYOffset > 300) {
+//         hello.style.display = "block";
+//     } else {
+//         hello.style.display = "none";
+//     }
+//   });
+
+//   hello.addEventListener("click", () => {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: "smooth"
+//     });
+//   });
+
+const hello = document.querySelector("#scorl-Button")
+window.addEventListener("")
+
